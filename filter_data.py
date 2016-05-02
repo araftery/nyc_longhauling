72426d918308cad7eccd7acd075eea6b1c827ec0
Andrew Raftery <andrewraftery@gmail.com>
2016-05-01 22:12:46 -0400

    add time info

--- filter_data.py
+++ filter_data.py
@@ -2,14 +2,16 @@ import pandas as pd
 from utils.data_cleaning import clean_taxi_data
 from progressbar import ProgressBar
 import json
+import time
 
 
 with open('to_keep.json', 'r') as infile:
     to_keep = set(json.load(infile))
 
-pbar = ProgressBar()
 
-for month_num in pbar(range(1, 13)):
+for month_num in range(1, 13):
+    print month_num
+    start_time = time.time()
     month_zfilled = unicode(month_num).zfill(2)
     df = pd.read_csv('data/yellow_tripdata_2015-{}.csv'.format(month_zfilled))
     df = clean_taxi_data(df)
@@ -38,3 +40,5 @@ for month_num in pbar(range(1, 13)):
     filtered['group_id'] = filtered['route'].str.cat(filtered['pickup_day_hour'], sep=' at ')
     filtered.to_csv('filtered_data/2015-{}'.format(month_zfilled))
 
+    print "Took {}s".format(time.time() - start_time)
+    print "\n\n"
